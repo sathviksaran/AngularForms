93eaecf8ce09fcbcbdefba6aae5069bcbfb7c02d
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2024-05-30 13:25:25 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,67 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularForms';
+  @ViewChild('myForm') form!: NgForm;
+  defaultCountry = 'india';
+
+  firstname!: string;
+  lastname!: string;
+  email!: string;
+  gen!: string;
+  country!: string;
+
+  defaultGender: string = 'Male';
+
+  gender = [
+    {id: '1', value: 'Male'},
+    {id: '2', value: 'Female'},
+    {id: '3', value: 'Other'}
+  ];
+
+  onSubmit(){
+    console.log(this.form);
+
+    this.firstname = this.form.value.personDetails.firstname;
+    this.lastname = this.form.value.personDetails.lastname;
+    this.email = this.form.value.personDetails.email;
+    this.gen = this.form.value.gender;
+    this.country = this.form.value.country;
+
+    this.form.reset();
+  }
+
+  setDefaultValues(){
+    // this.form.value.personDetails.firstname = 'John';
+    // this.form.value.personDetails.lastname = 'Smith';
+    // this.form.value.personDetails.email = 'abc@gmail.com';
+    // this.form.setValue({
+    //   country: '',
+    //   gender: '',
+    //   hobbies: '',
+    //   personDetails: {
+    //     firstname: 'John',
+    //     lastname: 'Smith',
+    //     email: 'abc@gmail.com'
+    //   }
+    // })
+
+    this.form.form.patchValue({
+      personDetails: {
+        firstname: 'John',
+        lastname: 'Smith',
+        email: 'abc@gmail.com'
+      }
+    })
+  }
 }
